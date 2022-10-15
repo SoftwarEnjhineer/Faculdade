f3c5056739abadef3cd1ee9c150d4252462f0457
SoftwarEnjhineer <110724659+SoftwarEnjhineer@users.noreply.github.com>
2022-10-15 11:31:16 -0300

    sai

--- Aula 9/ex3a.py
+++ Aula 9/ex3a.py
@@ -1,2 +1,24 @@
 # 3) Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:
 # a) Quantos números foram digitados.
+# b) A lista de valores, ordenada de forma decrescente.
+# c) Se o valor 5 foi digitado e está ou não na lista.
+
+lista = []
+
+escolha = 'S'
+while escolha == 'S':
+
+    n = input('Digite os números: ')
+    lista.append(n)
+    lista.sort()
+    lista.reverse()
+
+    escolha = input('Gostaria de continuar adicionando números? (s/n): ').upper()
+
+if '5' in lista:
+    print('Contém 5 na lista')
+else:
+    print('Não contém 5 na lista')
+
+print(len(lista))
+print(lista)
